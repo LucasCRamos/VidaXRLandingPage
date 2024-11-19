7be68e3542e9f88b86730c691f148fca72637169
LucasCRamos <lucasret222@gmail.com>
2024-11-19 13:46:46 -0300

    fix errors

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("ctaButton").addEventListener("click", function() {
-    alert("Saiba mais sobre o VidaXR e como ele pode revolucionar o ensino odontológico!");
-});
-
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Obrigado por entrar em contato! Responderemos em breve.");
-});
